18f4ecface65e41c3db3aaf4fa7a5f779b1cf477
Kenetar <kbogantes@gmail.com>
2020-04-16 21:46:26 -0600

    ImplementacionDb

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require ("mongoose");
+const encrypt = require ("mongoose-encryption");
 
 const app = express();
 
@@ -13,10 +14,14 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true, useUnifiedTopology:false});
 
-const userSchema={
+const userSchema= new mongoose.Schema({
     email: String,
     password : String,
-};
+});
+
+const secretWord = "ThisIsOurSecret";
+
+userSchema.plugin(encrypt,{secret:secretWord, encryptedFields:["password"]});
 
 const User = new mongoose.model("User",userSchema);
 
