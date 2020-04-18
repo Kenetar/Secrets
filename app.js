c695bf2a6b40c5ae72da32792925e5b2cd209ac6
Kenetar <kbogantes@gmail.com>
2020-04-17 21:57:47 -0600

    Se crea el archivo de variables se sesion

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require ("mongoose");
 const encrypt = require ("mongoose-encryption");
+require ("dotenv").config();
 
 const app = express();
 
@@ -21,7 +22,8 @@ const userSchema= new mongoose.Schema({
 
 const secretWord = "ThisIsOurSecret";
 
-userSchema.plugin(encrypt,{secret:secretWord, encryptedFields:["password"]});
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]});
 
 const User = new mongoose.model("User",userSchema);
 
@@ -88,4 +90,5 @@ app.post("/login", function(req,res){
 
 app.listen(3000, function(){
     console.log("servidor iniciado");
+    console.log(process.env.APIKEY);
 });
